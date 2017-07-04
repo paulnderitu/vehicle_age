eb5935da045404fa79cbc46dfc55d65d51288b23
Your Name <youremail@domain.com>
2017-07-04 16:53:15 +0300

    added dealerships and in them you can add a car

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,6 +5,7 @@ class Vehicle
       @make = make
       @model = model
       @year = year
+      @id = @@vehicles.length().+(1)
     end
 
     define_method(:make) do
@@ -17,6 +18,10 @@ class Vehicle
 
     define_method(:year) do
       @year
+    end
+
+    define_method(:id) do
+      @id
     end
 
     define_singleton_method(:all) do
@@ -40,4 +45,14 @@ class Vehicle
       american_cars = ["Chrysler", "Ford", "GM"]
       american_cars.include?(@make).&(self.age().<=(15))
     end
+
+    define_singleton_method(:find) do |identification|
+      found_vehicle = nil
+      @@vehicles.each() do |vehicle|
+        if vehicle.id().eql?(identification.to_i())
+          found_vehicle = vehicle
+        end
+        end
+        found_vehicle
+      end
 end
